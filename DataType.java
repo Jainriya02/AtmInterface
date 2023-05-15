c273d9a6bf3ff4eecb8ee8ee02a503caffdcae16
Jainriya02 <123864169+Jainriya02@users.noreply.github.com>
2023-05-15 19:26:44 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,22 @@
+/* data type in java
+   2 types
+   primitive- already exists -there are 8 in java
+   non-primitive- which user created (array,String etc)
+
+   value=(-2^n-1 to 2^n-1-1)
+ */
+
+public class DataType {
+    public static void main(String[] args) {
+        byte a = 1 ;   //1byte
+        short b=12;    //2byte
+        int c=122;     //4byte
+        char ch='a';   //2byte
+        float k= 0.2f;   //4byte
+        long h=12345689;   //8byte
+        double j=0.2222;   //8byte
+        boolean s=true;    //1byte
+
+
+    }
+}
